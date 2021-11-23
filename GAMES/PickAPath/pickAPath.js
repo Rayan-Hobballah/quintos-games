22d53669bcce696c9bab57384605edb42e2865f0
rayan_hobballah <rayanhobballah@icloud.com>
2021-11-23 18:07:57 +0400

    1.0.19

--- GAMES/PickAPath/pickAPath.js
+++ GAMES/PickAPath/pickAPath.js
@@ -9,6 +9,7 @@
 		// null in this case indicates the player cancelled out of the prompt
 
 		let msg = ''; // initialize message to empty string
+		let opt = [];
 
 		if (choice == -1) {
 			/* PART A0: Start your story! */
@@ -17,6 +18,7 @@
 				'1: Ask "Who is it?"\n\t' +
 				'2: Ignore it and keep coding\n\t' +
 				'3: Try to go to sleep';
+			opt = [1, 2, 3];
 		} else if (choice == 1) {
 			/* PART A1: continue the story */
 			msg =
@@ -24,6 +26,7 @@
 				'4. get past him\n\t' +
 				'5. wait for him to go\n\t' +
 				'6. go to the door right next to you';
+			opt = [4, 5, 6];
 		} else if (choice == 2 || choice == 3) {
 			msg = 'GAME OVER. They left you alone.';
 		} else if (choice == 4) {
@@ -36,12 +39,14 @@
 				'7. Drop water into the cage ten minutes straight\n\t' +
 				'8. Survive 2 crocodiles that are starving and will eat anything they see\n\t' +
 				'9. Survive 100000 black widows waiting to eat someone';
+			opt = [7, 8, 9];
 		} else if (choice == 7) {
 			msg =
 				'You act smart and realize that the water will just pour out of the cage. Then you find yourself in a room with three doors and you say “again  I thought I got out! “ you realize that it is a tough choice (well that’s what you thought). What would you take?\n\n\t' +
 				'10. $32979\n\t' +
 				'11. 1 bitcoin\n\t' +
 				'12. Just leave!';
+			opt = [10, 11, 12];
 		} else if (choice == 8) {
 			msg = 'as soon as they see you they come rushing to you and eat you GAME OVER YOU DIED';
 		} else if (choice == 9) {
@@ -53,18 +58,21 @@
 				'13. Sleep\n\t' +
 				'14. Do some coding\n\t' +
 				'15. Tell your family what happened';
+			opt = [13, 14, 15];
 		} else if (choice == 11) {
 			msg =
 				'the price bitcoin falls! you should have tooken the money. well at least you got out GAME OVER YOU GOT OUT AL:]\n\n\t' +
 				'13. Sleep\n\t' +
 				'14. Do some coding\n\t' +
 				'15. Tell your family what happened';
+			opt = [13, 14, 15];
 		} else if (choice == 12) {
 			msg =
 				'you realize you would have gotten out anyways. you are frustrated and wish you took the money\n\n\t' +
 				'13. Sleep\n\t' +
 				'14. Do some coding\n\t' +
 				'15. Tell your family what happened';
+			opt = [13, 14, 15];
 		} else if (choice == 13) {
 			msg =
 				'you go to bed terrified of what just happened and realized "if i wasn\'t so smart i would be dead" GAME OVER you sucessfully made it out alive\n\nthank you for playing!';
@@ -75,8 +83,20 @@
 			msg =
 				'you text your family and brag of how you were so smart to make it out alive and you tell them the whole story GAME OVER you sucessfully made it out alive\n\nthank you for playing!';
 		}
+
+		if (opt.length == 0) {
+			await alert(msg);
+			break;
+		}
+
 		// prompt the player to make choices
-		choice = await prompt(msg);
+		let input = await prompt(msg);
+
+		if (opt.includes(input)) {
+			choice = input;
+		} else {
+			await alert('you made an invalid choice');
+		}
 
 		/* PART B0: end the game if there are no more choices to make */
 
